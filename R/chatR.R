ab05a3227f49c6cb1ba62eff4f2a670e85176dd1
Joel Gombin <joel.gombin@gmail.com>
2017-07-17 14:37:32 +0200

    let's try another approach: we write a local file - allows to have the chat window already open, probably allows to reopen the file whenever needed.

--- R/chatR.R
+++ R/chatR.R
@@ -1,4 +1,13 @@
 #' @export
 chatR <- function() {
-        htmltools::html_print(htmltools::HTML("<h1 style='font-family:Arial, Helvetica, sans-serif'>Cliquez sur le logo en bas à droite pour obtenir de l'aide</h1><script src='https://cdn.slaask.com/chat.js'></script><BR/><script>_slaask.init('463d098847c4a580fdc9ed29ec2d2994');</script>"))
+  page <- file(paste0(tempdir(), "/chatR.html"))
+  toWrite <- c("<!DOCTYPE html>",
+               "<html>",
+               "<head>",
+               "<meta charset=\"utf-8\"/>",
+               "<script src='https://cdn.slaask.com/chat.js'></script><BR/><script>_slaask.init('463d098847c4a580fdc9ed29ec2d2994');",
+               "</script>")
+  writeLines(toWrite, page)
+  close(page)
+  rstudioapi::viewer(paste0(tempdir(), "/chatR.html#chat-open"))
 }
